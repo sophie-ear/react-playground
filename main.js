f4b969891e29d2053fbbb8ad19aec67cf6068ecc
Sophie <sophie.ear@gmail.com>
2022-03-15 11:41:50 +0100

    exo5 exemple clock

--- main.js
+++ main.js
@@ -34,4 +34,45 @@ function LastName(props) {
 
 const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+//ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
+  
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+  
+	componentWillUnmount() {
+	  	clearInterval(this.timerID);
+	}
+  
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+  
+	render() {
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Clock />,
+	document.getElementById('app')
+);
+
