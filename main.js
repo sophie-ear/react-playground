b82ae4017b40c5a9c43aff917133707acdc6d8ed
Sophie <sophie.ear@gmail.com>
2022-03-16 14:51:20 +0100

    corrigé horloge

--- main.js
+++ main.js
@@ -77,23 +77,66 @@ ReactDOM.render(<Clock />, document.querySelector('#app'));  */
 
 
 function Clock(props) {
-    React.useEffect(() => {
-        tick();        
-    }, []);
-    
-    const [date, setDate] = React.useState(new Date());
 
+	const [date, setDate] = React.useState(new Date());
+  	const [color, setColor] = React.useState('teal');
+  	const intervalRef = React.useRef();
+
+    React.useEffect(() => {
+        tick(); 
+		console.log(intervalRef);       
+    }, []);
+
+
+    
     const tick = () => {
-        setInterval(() => {
+        intervalRef.current = setInterval(() => {
             setDate(new Date())
         }, 1000)
     }
     
+    const changeColor = (event) => {
+		console.log(event);
+		// si on envoie l'argument event, alors on change la couleur et on sort de la fonction
+		if(event) {
+			setColor("black");
+			return;
+		}
 
+      setColor(`#${(Math.floor(Math.random() * 16777215)).toString(16)}`);
+    }
+
+	const resetColor = () => {
+		setColor("black");
+	}
+
+	const stopClock = () => {
+		clearInterval(intervalRef.current);
+	}
+
+
+	// On peut rappeler directement tick l.137
+	/*
+	const startClock = () => {
+		tick();
+	}
+	*/
+
+	// A partir du moment où on appelle une fonction dans un gestionnaire d'évènement il faut l'appeler avec une fonction anonyme (avec "() =>" )
     return (
         <div>
             <h1>Hello world</h1>
-            <h2>Il est {date.toLocaleTimeString()}.</h2>
+            <h2 style={{ color }}>Il est {date.toLocaleTimeString()}.</h2>
+			<div>
+				<button onClick={ () => changeColor() }>Change color</button>
+				<button onClick={ () => changeColor(true) }>Reset color</button>
+			</div>
+
+			<div>
+				<button onClick={ stopClock }>Stop clock</button>
+				<button onClick={ tick }>Start clock</button>
+			</div>
+            
         </div>
     );
 }
