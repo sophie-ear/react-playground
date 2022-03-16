64c811c61acdfffab2e8d836bc132b99ea40c789
Sophie <sophie.ear@gmail.com>
2022-03-16 17:20:34 +0100

    exo8 done

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function UserGreeting(props) {
     return <h1>Bienvenue !</h1>;
   }
@@ -37,3 +38,63 @@ function UserGreeting(props) {
     <Greeting />,
     document.querySelector('#app')
   );
+  */
+
+
+/*const list = ['a', 'b', 'c', 'd'];
+
+const App = (props) => {
+	  
+	return (
+		<section>
+			{
+				list.map((letter, index) => <p key={index}>{letter}</p>)
+			}
+		</section>
+	)
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#app')
+);
+*/
+
+
+function App(props) {
+    React.useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(usersObject => setUsers(usersObject))
+    }, [])
+
+    const [users, setUsers] = React.useState([]);
+
+
+
+    return(
+        <React.Fragment>
+        {
+            users.map((u) =>(
+                <UserCard key={u.id} user={u}/>
+            ))
+        }
+        </React.Fragment>
+    )
+}
+
+function UserCard({user}) {
+    
+
+    return (
+        <ul className="user-card">
+            <li>{user.name}</li>
+            <li>{user.email}</li>
+            <li>{user.company.name}</li>
+            <li>{user.phone}</li>
+            <li>{user.website}</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'))
